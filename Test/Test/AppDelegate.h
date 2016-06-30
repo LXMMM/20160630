d73a89af758550b80c5b754bf09e761a7cacda7a
zhangsan <zhangsan@163.com>
2016-06-30 16:24:13 -0700

    qwe

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+///Users/mac/Desktop/GIT/20160630/Test/Test
+//  AppDelegate.h
+//  Test
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 lxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
